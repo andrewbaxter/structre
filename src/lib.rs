97155bec1cb0ed5290de81bfba9216a52d970807
andrew <>
2023-07-19 23:33:57 +0900

    Remove dep on anyhow, once_cell; better qualify dep modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,3 @@
-pub use anyhow::{Context, Error, Result};
-pub use once_cell::sync::Lazy;
 pub use structre_proc_macros::structre;
-
 #[cfg(feature = "unicode")]
 pub use regex::Regex as UnicodeRegex;
--- src/proc_macros/mod.rs
+++ src/proc_macros/mod.rs
@@ -1,10 +1,22 @@
-use std::collections::{HashMap, HashSet};
-
+use std::collections::{
+    HashMap,
+    HashSet,
+};
 use litrs::StringLit;
 use proc_macro2::TokenStream;
-use quote::{format_ident, quote, ToTokens};
+use quote::{
+    format_ident,
+    quote,
+    ToTokens,
+};
 use regex_syntax::hir::Hir;
-use syn::{self, parse_macro_input, DataStruct, Ident, Type};
+use syn::{
+    self,
+    parse_macro_input,
+    DataStruct,
+    Ident,
+    Type,
+};
 
 struct Data<'a> {
     captures: Vec<&'a Hir>,
@@ -27,49 +39,45 @@ impl<'a> Data<'a> {
                         panic!("ASSERTION cap len {} but index {}", self.captures.len(), i)
                     }
                     self.captures.push(&g.hir);
-                }
+                },
                 regex_syntax::hir::GroupKind::CaptureName { name, index } => {
                     let index = *index as usize - 1;
                     if self.captures.len() != index {
-                        panic!(
-                            "ASSERTION cap len {} but index {}",
-                            self.captures.len(),
-                            index
-                        )
+                        panic!("ASSERTION cap len {} but index {}", self.captures.len(), index)
                     }
                     self.captures.push(&g.hir);
                     self.named_captures.insert(name.clone(), index);
-                }
+                },
                 regex_syntax::hir::GroupKind::NonCapturing => self.walk_re(&g.hir),
             },
             regex_syntax::hir::HirKind::Concat(c) => {
                 for c in c {
                     self.walk_re(c);
                 }
-            }
+            },
             regex_syntax::hir::HirKind::Alternation(_) => (),
         }
     }
 
-    fn gen_struct_tuple(
-        &self,
-        i: &mut usize,
-        fields: &mut dyn Iterator<Item = &Type>,
-    ) -> Vec<TokenStream> {
+    fn gen_struct_tuple(&self, i: &mut usize, fields: &mut dyn Iterator<Item = &Type>) -> Vec<TokenStream> {
         let mut out = vec![];
         for ty in fields {
             match ty {
                 Type::Tuple(t) => {
                     let child = self.gen_struct_tuple(i, &mut t.elems.iter());
-                    out.push(quote!((#(#child),*)));
-                }
+                    out.push(quote!((#(#child), *)));
+                },
                 ty => {
-                    let err_lit = format!("Failed to parse field {}", i);
+                    let err_lit = format!("Failed to parse field {}: {{}}", i);
                     *i += 1;
                     out.push(
-                        quote!(#ty::from_str(caps_.get(#i).map(|m| m.as_str()).unwrap_or("")).context(#err_lit)?),
+                        quote!(
+                            < #ty as std:: str:: FromStr >:: from_str(
+                                caps_.get(#i).map(|m| m.as_str()).unwrap_or("")
+                            ).map_err(|e| format!(#err_lit, e)) ?
+                        ),
                     );
-                }
+                },
             }
         }
         out
@@ -79,12 +87,8 @@ impl<'a> Data<'a> {
         match &d.fields {
             syn::Fields::Named(n) => {
                 let mut field_tokens = vec![];
-                let mut keys = self
-                    .named_captures
-                    .keys()
-                    .into_iter()
-                    .map(&String::to_string)
-                    .collect::<HashSet<String>>();
+                let mut keys =
+                    self.named_captures.keys().into_iter().map(&String::to_string).collect::<HashSet<String>>();
                 for field in &n.named {
                     let name = field.ident.as_ref().unwrap();
                     let i = match self.named_captures.get(&name.to_string()) {
@@ -93,10 +97,14 @@ impl<'a> Data<'a> {
                     };
                     keys.remove(&name.to_string());
                     let ty = &field.ty;
-                    let err_lit = format!("Failed to parse field {}", name);
+                    let err_lit = format!("Failed to parse field {}: {{}}", name);
                     let i = i + 1;
                     field_tokens.push(
-                        quote!(#name: #ty::from_str(caps_.get(#i).map(|m| m.as_str()).unwrap_or("")).context(#err_lit)?),
+                        quote!(
+                            #name:< #ty as std:: str:: FromStr >:: from_str(
+                                caps_.get(#i).map(|m| m.as_str()).unwrap_or("")
+                            ).map_err(|e| format!(#err_lit, e)) ?
+                        ),
                     );
                 }
                 if !keys.is_empty() {
@@ -106,35 +114,27 @@ impl<'a> Data<'a> {
                     panic!("This is a struct with named fields but there are some unused unnamed captures");
                 }
                 quote!(Ok(#ident {
-                    #(#field_tokens),*
+                    #(#field_tokens),
+                    *
                 }))
-            }
+            },
             syn::Fields::Unnamed(u) => {
                 if !self.named_captures.is_empty() {
-                    panic!(
-                        "Tuples must have only unnamed captures, but named captures are present"
-                    );
+                    panic!("Tuples must have only unnamed captures, but named captures are present");
                 }
                 let mut i = 0usize;
-                let field_tokens =
-                    self.gen_struct_tuple(&mut i, &mut u.unnamed.iter().map(|e| &e.ty));
+                let field_tokens = self.gen_struct_tuple(&mut i, &mut u.unnamed.iter().map(|e| &e.ty));
                 if i != self.captures.len() {
-                    panic!(
-                        "Struct has {} fields but only {} captures",
-                        u.unnamed.len(),
-                        self.captures.len()
-                    );
+                    panic!("Struct has {} fields but only {} captures", u.unnamed.len(), self.captures.len());
                 }
-                quote!(Ok(#ident (
-                    #(#field_tokens),*
-                )))
-            }
+                quote!(Ok(#ident(#(#field_tokens), *)))
+            },
             syn::Fields::Unit => {
                 if !self.captures.is_empty() {
                     panic!("This is an empty struct but regex has captures")
                 }
-                quote!(Ok(#ident ()))
-            }
+                quote!(Ok(#ident()))
+            },
         }
     }
 }
@@ -160,20 +160,15 @@ fn gen_impls(regex_raw: &str, ast: syn::DeriveInput) -> TokenStream {
     let name_parser = format_ident!("{}FromRegex", name);
     let mut out = vec![ast.to_token_stream()];
     #[cfg(feature = "unicode")]
-    out.push(quote! {
+    out.push(quote!{
         #vis struct #name_parser(structre::UnicodeRegex);
-
         impl #name_parser {
             #vis fn new() -> Self {
-                Self(structre::UnicodeRegex::new(#regex_raw).unwrap())
+                Self(structre:: UnicodeRegex:: new(#regex_raw).unwrap())
             }
-
-            #vis fn parse(&self, input: &str) -> structre::Result<#name> {
-                #[allow(unused_imports)]
-                use std::str::FromStr;
-                #[allow(unused_imports)]
-                use structre::Context;
-                let caps_ = self.0.captures(input).ok_or_else(|| structre::Error::msg("No match"))?;
+            #vis fn parse(&self, input: &str) -> Result < #name,
+            String > {
+                let caps_ = self.0.captures(input).ok_or_else(|| ToString::to_string("No match"))?;
                 #value
             }
         }
@@ -182,10 +177,7 @@ fn gen_impls(regex_raw: &str, ast: syn::DeriveInput) -> TokenStream {
 }
 
 #[proc_macro_attribute]
-pub fn structre(
-    args: proc_macro::TokenStream,
-    body: proc_macro::TokenStream,
-) -> proc_macro::TokenStream {
+pub fn structre(args: proc_macro::TokenStream, body: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let mut args = proc_macro2::TokenStream::from(args).into_iter();
     let regex_raw = match args.next().unwrap() {
         proc_macro2::TokenTree::Literal(l) => match StringLit::try_from(&l) {
@@ -204,9 +196,7 @@ pub fn structre(
 #[cfg(test)]
 mod tests {
     use std::str::FromStr;
-
     use proc_macro2::TokenStream;
-
     use crate::gen_value;
     use quote::quote;
 
@@ -216,13 +206,16 @@ mod tests {
             gen_value(
                 "(a)",
                 &syn::parse2(TokenStream::from_str("struct Parsed(String);").unwrap()).unwrap(),
-            )
-            .to_string(),
-            quote!(Ok(Parsed(
-                String::from_str(caps_.get(1usize).map(|m| m.as_str()).unwrap_or(""))
-                    .context("Failed to parse field 0")?
-            )))
-            .to_string()
+            ).to_string(),
+            quote!(
+                Ok(
+                    Parsed(
+                        String::from_str(
+                            caps_.get(1usize).map(|m| m.as_str()).unwrap_or(""),
+                        ).context("Failed to parse field 0")?,
+                    ),
+                )
+            ).to_string()
         );
     }
 
@@ -231,17 +224,22 @@ mod tests {
         assert_eq!(
             gen_value(
                 "(a)(b)",
-                &syn::parse2(TokenStream::from_str("struct Parsed((String, u32));").unwrap())
-                    .unwrap(),
-            )
-            .to_string(),
-            quote!(Ok(Parsed((
-                String::from_str(caps_.get(1usize).map(|m| m.as_str()).unwrap_or(""))
-                    .context("Failed to parse field 0")?,
-                u32::from_str(caps_.get(2usize).map(|m| m.as_str()).unwrap_or(""))
-                    .context("Failed to parse field 1")?
-            ))))
-            .to_string()
+                &syn::parse2(TokenStream::from_str("struct Parsed((String, u32));").unwrap()).unwrap(),
+            ).to_string(),
+            quote!(
+                Ok(
+                    Parsed(
+                        (
+                            String::from_str(
+                                caps_.get(1usize).map(|m| m.as_str()).unwrap_or(""),
+                            ).context("Failed to parse field 0")?,
+                            u32::from_str(
+                                caps_.get(2usize).map(|m| m.as_str()).unwrap_or(""),
+                            ).context("Failed to parse field 1")?,
+                        ),
+                    ),
+                )
+            ).to_string()
         );
     }
 
@@ -250,17 +248,16 @@ mod tests {
         assert_eq!(
             gen_value(
                 "(?P<a>a)(?P<b>b)",
-                &syn::parse2(TokenStream::from_str("struct Parsed { b: u32, a: String }").unwrap())
-                    .unwrap(),
-            )
-            .to_string(),
+                &syn::parse2(TokenStream::from_str("struct Parsed { b: u32, a: String }").unwrap()).unwrap(),
+            ).to_string(),
             quote!(Ok(Parsed {
-                b: u32::from_str(caps_.get(2usize).map(|m| m.as_str()).unwrap_or(""))
-                    .context("Failed to parse field b")?,
-                a: String::from_str(caps_.get(1usize).map(|m| m.as_str()).unwrap_or(""))
-                    .context("Failed to parse field a")?
-            }))
-            .to_string()
+                b: u32::from_str(
+                    caps_.get(2usize).map(|m| m.as_str()).unwrap_or(""),
+                ).context("Failed to parse field b")?,
+                a: String::from_str(
+                    caps_.get(1usize).map(|m| m.as_str()).unwrap_or(""),
+                ).context("Failed to parse field a")?,
+            })).to_string()
         );
     }
 }
